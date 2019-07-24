4625a388c64ba727e71e67b6d0965d0edd6e9d78
helenkota <helenr.kota@gmail.com>
2019-07-24 19:53:59 -0400

    Made first line

---
+++ Adventure_Game_2.py
@@ -0,0 +1,4 @@
+answer = input('You have just been whisked away to a faraway island. A lady wearing a fancy dress is walking toward you. Walk away(1) or stay(2). ')
+if answer == '1':
+    print('Suddenly, a big portal opens in front of you and you come back to Earth.You live a normal life. MISSION FAILED!')
+    
