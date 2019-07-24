98d53ebf2fc33c45b41af4f1cc16f0e58c87374b
helenkota <helenr.kota@gmail.com>
2019-07-24 16:44:25 -0400

    added score

--- word game.py
+++ word game.py
@@ -1,4 +1,8 @@
 #Helen Kota 2019
 last_letter = 'a'
 score = 0
-user_word = input("Enter a word starting with" + " last_letter " 
+user_word = input("Enter a word starting with " + last_letter)
+if user_word.startswith(last_letter):
+    score = score+1
+    print('score=')
+    print(score)
