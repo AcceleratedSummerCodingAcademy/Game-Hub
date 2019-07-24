6d4912ec7324c42747e0f9e0909df15be3324722
helenkota <helenr.kota@gmail.com>
2019-07-24 19:19:53 -0400

    Made High score feture

--- word game.py
+++ word game.py
@@ -1,12 +1,19 @@
 #Helen Kota 2019
 last_letter = 'a'
 score = 0
-user_word = input("Enter a word starting with " + last_letter)
-if user_word.startswith(last_letter):
-    score = score+1
-    print('score=')
-    print(score)
-else:
-    print('score=')
-    print(score)
+used_words=[]
+while True:
+    user_word = input("Enter a word starting with " + last_letter + ': ')
+    if not user_word.startswith(last_letter):
+        print('Wrong letter! Try again next time!')
+        break
+    elif user_word in used_words :
+        print('You reused a word.Game over!')
+        break
+    else:
+        score = score+1
+        print('score=' + str(score) + '!OOT!')
+        last_letter = user_word[-1]
+        used_words.append(user_word)
     
+print('Your High score is ' + str(score) + '!') 
