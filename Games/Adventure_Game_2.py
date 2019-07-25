64706dd8ef87fca66a458ecd89585f3c219ff56e
helenkota <helenr.kota@gmail.com>
2019-07-25 12:02:08 -0400

    Added dragon

--- Games/Adventure_Game_2.py
+++ Games/Adventure_Game_2.py
@@ -10,7 +10,15 @@ def run():
     if answer == '1':
         answer == input('She gives you a choice of supplies. Rope(1) or Flintstone(2)')
         if answer == '1':
+            answer = input('You take the rope and proceed. Suddenly, a dragon with a diamond attacks you. Fight(1) or run(2)?')
+            if answer == '1':
+                print('You fight the dragon, but he is too strong. He kills you. MISSION FAILED!')
+            if answer == '2':
+                input('You run away.')
             
+        if answer == '2':
+            print('You take the flint  and proceed. You do not realize, but the flint falls out of your pocket and rubs on a stone. A fire starts. MISSION FAILED!')  
             
     elif answer == '2':
         print('The lady waves her hand. Suddenly, a big portal opens in front of you and you come back to Earth.You live a normal life. MISSION FAILED!') 
+run()
