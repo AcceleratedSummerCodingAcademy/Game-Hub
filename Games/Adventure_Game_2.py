58e5ca7433ff29936f76a462cbbe08dd88ca9715
helenkota <helenr.kota@gmail.com>
2019-07-24 20:06:36 -0400

    Added choice 2

--- Games/Adventure_Game_2.py
+++ Games/Adventure_Game_2.py
@@ -1,4 +1,5 @@
 answer = input('You have just been whisked away to a faraway island. A lady wearing a fancy dress is walking toward you. Walk away(1) or stay(2). ')
 if answer == '1':
     print('Suddenly, a big portal opens in front of you and you come back to Earth.You live a normal life. MISSION FAILED!')
-    
+elif answer == '2':
+    answer = input('The lady says that a precious gem has been stolen from the island. What do you say? Yes(1), or No(2). ')
